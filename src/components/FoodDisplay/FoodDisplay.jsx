350a748ea28e255f7c65df8820d10c442c42fbc5
[f.zahra] <fatima.azekraoui@dci-student.org>
2024-10-23 22:33:26 +0200

    mod component fooddisplay

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import './FoodDisplay.css';
 import { StoreContext } from '../../context/StoreContext';
+import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
   const {
@@ -15,29 +16,22 @@ const FoodDisplay = ({ category }) => {
     pastilla_list
   } = useContext(StoreContext);
 
-  // Function to select the correct list based on the category
+  // Fonction pour sélectionner la liste de nourriture correcte en fonction de la catégorie
   const getFoodList = (category) => {
-    switch(category) {
-      case 'entree':
-        return entree_list;
-      case 'tajine':
-        return tajine_list;
-      case 'couscous':
-        return couscous_list;
-      case 'dessert':
-        return dessert_list;
-      case 'drink':
-        return drink_list;
-      case 'bread':
-        return bread_list;
-      case 'soup':
-        return soup_list;
-      case 'grill':
-        return grill_list;
-      case 'pastilla':
-        return pastilla_list;
-      default:
-        return [];
+    switch (category) {
+      case 'All':
+        return [
+          ...entree_list, 
+          ...tajine_list, 
+          ...couscous_list, 
+          ...dessert_list, 
+          ...drink_list, 
+          ...bread_list, 
+          ...soup_list, 
+          ...grill_list, 
+          ...pastilla_list
+        ];
+      
     }
   };
 
@@ -48,11 +42,14 @@ const FoodDisplay = ({ category }) => {
       <h2>Feinste Spezialitäten direkt bei Ihnen</h2>
       <div className="food-display-list">
         {foodList.map((item, index) => (
-          <div key={index} className="food-item">
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <span>{item.price}</span>
-          </div>
+          <FoodItem 
+            key={index} 
+            id={item._id} 
+            name={item.name} 
+            description={item.description} 
+            price={item.price} 
+            image={item.image} 
+          />
         ))}
       </div>
     </div>
---
+++ src/components/FoodItem/FoodItem.jsx
@@ -0,0 +1,24 @@
+import './FoodItem.css'
+
+
+const FoodItem = ({name, price, description, image}) => {
+  return (
+    <div className='food-item'>
+        <div className="food-item-img-container">
+            <img className='food-item-image' src={image} alt="" />
+        </div>
+        <div className="food-item-info">
+            <div className="food-item-name-rating">
+                <p>{name}</p>
+                {/*<img src={assets.rating_starts} alt="" />*/}
+            </div>
+            <p className="food-item-desc">{description}</p>
+            <p className="food-item-price">{price}€</p>
+        </div>
+
+      
+    </div>
+  )
+}
+
+export default FoodItem
